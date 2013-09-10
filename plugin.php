e518eb912dcb6e6f29f812384bc2c0f57af7ca43
Beanow <beanow@project-virality.com>
2013-09-10 12:52:30 +0200

    Initial version.

---
+++ plugin.php
@@ -0,0 +1,3 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.');
+require_once($path.DS.'src'.DS.'Michelf'.DS.'Markdown.php');
+require_once($path.DS.'src'.DS.'Michelf'.DS.'MarkdownExtra.php');
